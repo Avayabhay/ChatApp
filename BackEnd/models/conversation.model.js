ed6440db9d8e00462845e95f6df8d170d0b95dba
Abhay Sharma <avay.abhay@gmail.com>
2024-04-29 22:06:01 +0530

    created Conversation Model

---
+++ BackEnd/models/conversation.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const conversationSchema = mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+        default: [],
+      },
+    ],
+  },
+  { timeStamp: true }
+);
+
+const Conversation = mongoose.Model("Conversation", conversationSchema);
+export default Conversation;
