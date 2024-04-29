d142151d8287734cf9155c3235dd948284080763
Abhay Sharma <avay.abhay@gmail.com>
2024-04-29 21:13:53 +0530

    created Message model

---
+++ BackEnd/models/message.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } //created at and updated at fields
+);
+
+const Message = mongoose.Model("Message", messageSchema);
+
+export default Message;
