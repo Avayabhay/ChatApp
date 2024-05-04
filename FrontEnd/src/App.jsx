fcd2a98cbf4e7a7d7ffe3b706ec2ad9eb493683f
Abhay Sharma <avay.abhay@gmail.com>
2024-05-04 16:25:28 +0530

    completed the chats UI
    Completed the UI responsible for showing chats and conversations

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 // import Login from "./pages/login/Login";
-import SignUp from "./pages/signUp/SignUp";
-// import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+// import SignUp from "./pages/signUp/SignUp";
+import Home from "./pages/home/Home";
+// import Login from "./pages/login/Login";
+// import Home from "./pages/home/home";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center u">
-      <SignUp />
+      <Home />
     </div>
   );
 }
